a5e0fe972ccf4d74d1fb3d337fcc339fbe4e6f28
SnappyCarp <126493503+SnappyCarp@users.noreply.github.com>
2023-03-08 05:53:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ with open('Authorization.json', 'r') as f:
 class ScreenshareSelfbot(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
+        #Put Your Userid Here ↓
+        self.AuthorId = 1
 
     async def on_message(self, message):
         if message.author==self.user: return
-        if message.content == 'ping' and message.author.id==904144997559975937:
+        if message.content == 'ping' and message.author.id==self.AuthorId:
             await message.channel.send('pong')
 
 
