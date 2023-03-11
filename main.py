b4482f1a61a863c98eddd08e8820eadb1789dd04
SnappyCarp <126493503+SnappyCarp@users.noreply.github.com>
2023-03-11 14:55:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-import json
+from threading import Thread
+from discord.errors import HTTPException
+from selenium import webdriver
 import discord
+import json
 
+with open('Authorization.json', 'r') as f: data=json.load(f); Prefix:str = data['Prefix']
 
-
-with open('Authorization.json', 'r') as f:
-    data=json.load(f)
-    Token=data['Token']
 
 class ScreenshareSelfbot(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
-        #Put Your Userid Here ↓
-        self.AuthorId = 1
+        print('Logged on as', self.user, 'Bot is Ready For Use')
 
     async def on_message(self, message):
+        #don't respond to self
         if message.author==self.user: return
-        if message.content == 'ping' and message.author.id==self.AuthorId:
-            await message.channel.send('pong')
+
+        #commands
+        #ping command
+        if message.content == Prefix+'ping':
+            await message.channel.send(f'Client Ping Is {self.latency*1000}ms')
+            print(message.author.name, 'Used The Command Ping')
+        #start streaming
+        elif message.content.startswith(Prefix+'Video'):
+            YtUrl = message.content.replace(Prefix, '').replace('Video', '')
+            try:
+                await message.channel.send('Loading Video')
+            except HTTPException:
+                await message.channel.send('Please Send A Youtube Url')
+            
 
 
-ScreenshareSelfbot().run(Token)
+
+Thread(target=ScreenshareSelfbot().run(data['Token'])).start()
